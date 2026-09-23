0e062a7c2e7c01433a2941d0fd8c94cbd56a65ff
Maximiliano Rao <maxism96@gmail.com>
2026-09-23 17:34:20 -0300

    Configuración para Neon

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -5,4 +5,5 @@ dotenv.config();
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes('neon.tech') ? { rejectUnauthorized: false } : false,
 });
